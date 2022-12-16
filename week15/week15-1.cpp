0be0881c71746b84adf8aea26b7cffe1d87eb739
yuchiling <11160091@me.mcu.edu.tw>
2022-12-16 11:58:08 +0800

    week15

---
+++ week15/week15-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+
+        printf("Hello World\n");
+        char line[] = "Hello World";
+        printf("整數 %d\n", 100);
+        printf("浮點數 %f\n", 3.14159265358979);
+        printf("%s 字串\n", line);
+
+}
